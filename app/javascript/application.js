caf0ed94fe230a266ad6a1000a06f5fcb12623f2
Jerome <fabricantejerome@gmail.com>
2023-12-19 08:05:22 +0800

    Add admin template

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import Alpine from "alpinejs";
+import persist from '@alpinejs/persist'
+
+Alpine.plugin(persist)
+window.Alpine = Alpine;
+Alpine.start();
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "alpinejs" # @3.13.3
+pin "@alpinejs/persist", to: "@alpinejs--persist.js" # @3.13.3
